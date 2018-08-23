715b3464216097b4b2f8bddbb108fd88a5f1d731
DallasO <8305525+DallasO@users.noreply.github.com>
2018-08-22 22:14:03 -0700

    First publish of script
    This is the initial commit of my script.

---
+++ ClamAVCron.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# @(#)$Id$
+#
+#
+# Scan direcories for viruses
+#
+# ************************
+# *To run with Anacron*
+# or to add to /etc/cron.*
+# -- cron.daily
+# -- cron.weekly
+# -- cron.monthly
+# this file cannot end with .sh or runparts will not run it.
+# For this, you will also need to make the file executable and owned by root
+# chmod +x ClamAVCron && chown root:root ClamAVCron
+# ************************
+#
+# ************************
+# *To run with Cron or Anacron*
+# You may want to replace the $USER to your preferred username
+# ************************
+
+
+logdate=$(date "+%b-%d-%Y")
+logfile="/home/$USER/.clamtk/history/${logdate}.log"
+scandir="/home/$USER"
+
+# notif - Start Scan
+#zenity --notification --text="ClamAV scan has been started"
+
+clamscan -ir -l $logfile $scandir &> /dev/null
+
+chown $USER:$USER $logfile
+
+# notif - Scan Finished
+#zenity --notification --text="ClamAV scan has finished"
