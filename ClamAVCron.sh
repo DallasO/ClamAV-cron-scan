aa70b09ea10b8814681f7785fadcedb0a770c138
DallasO <8305525+DallasO@users.noreply.github.com>
2019-01-11 23:13:00 -0800

    Run scan in userspace anacron instance with instructions

--- ClamAVCron.sh
+++ ClamAVCron.sh
@@ -5,24 +5,38 @@
 #
 # Scan direcories for viruses
 #
-# ************************
-# *To run with Anacron*
-# or to add to /etc/cron.*
-# -- cron.daily
-# -- cron.weekly
-# -- cron.monthly
-# this file cannot end with .sh or runparts will not run it.
-# For this, you will also need to make the file executable and owned by root
-# chmod +x ClamAVCron && chown root:root ClamAVCron
-# ************************
+# ****************************************************************************************************
+# The preferred method of using this script is with a separate anacron instance running in user mode
+# Credit to askubuntu answer | https://askubuntu.com/a/235090/611842
+# ****************************************************************************************************
 #
-# ************************
-# *To run with Cron or Anacron*
-# You may want to replace the $USER to your preferred username
-# eg. $USER=username
-USER=$USER
-# ************************
+# 1. Create a `.anacron` folder in your home directory and in it two subfolders, `etc` and `spool`:
+#
+#     $ mkdir -p ~/.anacron/{etc,spool}
+#
+# 2. Create a new file `~/.anacron/etc/anacrontab` with contents similar to the following:
+#
+#         # /etc/anacrontab: configuration file for anacron
+#
+#         # See anacron(8) and anacrontab(5) for details.
+#
+#         SHELL=/bin/bash
+#         PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+#
+#         # period  delay  job-identifier  command
+#         1         10     clamav-scan     ClamAVCron.sh
+#
+# 3. By using:
+#
+#      $ crontab -e
+#
+#   Add the following line:
+#
+#      @hourly /usr/sbin/anacron -s -t $HOME/.anacron/etc/anacrontab -S $HOME/.anacron/spool
+#
+# ****************************************************************************************************
 
+USER=$USER
 
 logdate=$(date "+%b-%d-%Y")
 scandir="/home/$USER/"
@@ -33,8 +47,6 @@ ignoredir="${scandir}.local/share/Trash/"
 #zenity --notification --text="ClamAV scan has been started"
 
 clamscan -ir -l $logfile --exclude-dir=$ignoredir $scandir &> /dev/null
-
-chown $USER:$USER $logfile
 
 # notif - Scan Finished
 #zenity --notification --text="ClamAV scan has finished"
--- README.md
+++ README.md
@@ -4,7 +4,9 @@ This is a custom script that allows a user to scan their `~/home` folder using a
 
 View the [ClamAV Github page](https://www.clamav.net/).
 
-## Installation
+---
+
+## ClamAV Installation
 
 Follow the recommended installation steps for your distro on their [documentation](https://www.clamav.net/documents/installing-clamav) page.
 
@@ -12,6 +14,38 @@ For an interactive gui, also install their *clamtk* package.
 
 ### Debian
 `# apt install clamav clamtk`
+
+---
+
+## How to install this script
+
+### Using Anacron (preferred method)
+[Credit to askubuntu answer](https://askubuntu.com/a/235090/611842)
+<!-- language-all: bash -->
+
+You can set up a separate anacron instance to run in user mode:
+
+1. Create a `.anacron` folder in your home directory and in it two subfolders, `etc` and `spool`:
+
+       mkdir -p ~/.anacron/{etc,spool}
+
+2. Create a new file `~/.anacron/etc/anacrontab` with contents similar to the following:
+
+        # /etc/anacrontab: configuration file for anacron
+
+        # See anacron(8) and anacrontab(5) for details.
+
+        SHELL=/bin/bash
+        PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+        # period  delay  job-identifier  command
+        1         10     clamav-scan     ClamAVCron.sh
+
+3. Add the following line to your crontab using `crontab -e`:
+
+        @hourly /usr/sbin/anacron -s -t $HOME/.anacron/etc/anacrontab -S $HOME/.anacron/spool
+
+---
 
 ## Planned features
 * Catch errors, such as *clamav* not installed
