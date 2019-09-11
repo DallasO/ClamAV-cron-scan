9833f8c25b42d0268a5121f41fa38a9282dca6e9
Dallas Opelt <8305525+DallasO@users.noreply.github.com>
2019-09-11 11:09:13 -0700

    feat: get stable and manage own spool (#4)

--- ClamAVCron.sh
+++ ClamAVCron.sh
@@ -5,50 +5,26 @@
 #
 # Scan direcories for viruses
 #
-# ****************************************************************************************************
-# The preferred method of using this script is with a separate anacron instance running in user mode
-#
-# ****************************************************************************************************
+#******************************************************************************************
+# You can set an hourly cron for a laptop, or weekly/monthly for servers
+#******************************************************************************************
 #
 # 1. Make sure this script is executable
 #     $ chmod u+x ClamAVCron.sh
 #
-# 2. To allow Anacron to execute the file, symlink it in /usr/local/bin (see run-parts), or remove the .sh extension
-#    This will also allow you to scan directly from your terminal
+# 2. If you would like to invoke this script at any time from the terminal,
+#    link it in /usr/local/bin (see `run-parts`)
+#
 #     # ln -s /path/to/ClamAVCron.sh /usr/local/bin/clamscan-cron
 #
-# 3. Create a `.anacron` folder in your home directory and in it two subfolders, `etc` and `spool`:
-#
-#     $ mkdir -p ~/.anacron/{etc,spool}
-#
-# 4. Create a new file `~/.anacron/etc/anacrontab` with contents similar to the following, insert your username in place of `<username>`:
-#
-#         # /etc/anacrontab: configuration file for anacron
-#
-#         # See anacron(8) and anacrontab(5) for details.
-#
-#         SHELL=/bin/bash
-#         PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-#
-#         # period:         Delay in days
-#         # delay:          Delay in minutes
-#         # job-identifier: Unique name for anacron
-#         # command:        Command to execute
-#         # period  delay  job-identifier  command
-#         1         10     clamav-scan     /path/to/ClamAVCron <username>
-#         # OR
-#         1         10     clamav-scan     /usr/local/bin/clamscan-cron <username>
-#
-# 5. By using:
-#
-#      $ crontab -e
-#
-#   Add the following line:
+# 3. Add the following line to your crontab using `crontab -e`:
 #
 #      @hourly /usr/sbin/anacron -s -t $HOME/.anacron/etc/anacrontab -S $HOME/.anacron/spool
 #
-# ****************************************************************************************************
-set -u ;
+#******************************************************************************************
+set -o nounset ;
+set -o errexit ;
+set -o xtrace  ;
 
 # Make sure ClamAV is installed
 if ! type -p /usr/bin/clamscan &> /dev/null ; then
@@ -57,45 +33,78 @@ if ! type -p /usr/bin/clamscan &> /dev/null ; then
   exit 1 ;
 fi
 
-if /usr/bin/id -u $1 > /dev/null 2>&1; then
+if /usr/bin/id -u "$1" > /dev/null 2>&1; then
   USER=$1 ;
 else
   /bin/echo "Valid user required." ;
   /bin/echo "Please use EXAMPLE:" ;
   /bin/echo "\$ $0 $USER" ;
-  if [ $1 != 'test' ]; then
+  if [ "$1" != 'test' ]; then
     exit 1 ;
   fi
 fi
 
-if ! type -p /usr/bin/on_ac_power &> /dev/null; then
-  # Assume always on AC if on_ac_power doesn't exist
-  onAC=0 ;
+# How oten should we scan? default: 7 days
+scriptdir="$(dirname "$(readlink -f "$0")")"
+lockfile="$scriptdir/spool" ;
+recur=${2-7} ;
+
+if ( set -o noclobber; date "+%Y-%m-%d" > "$lockfile") 2> /dev/null; 
+then
+   trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT ;
 else
-  onAC=$(/usr/bin/on_ac_power; echo $?;) ;
+  lastrun=$( cat "$lockfile" ) ;
+  if (( ($(date +%s) - $(date --date="$lastrun" +%s) )/(60*60*24) > recur )); then
+    date "+%Y-%m-%d" > "$lockfile" ;
+  else
+    echo "We don't have to run" ;
+    exit 0 ;
+  fi
 fi
 
-if [ $onAC -ne 0 ]; then
-  /bin/echo "Not on AC Power. Skipping." ;
-  exit 1 ;
-fi
+logdate=$(date "+%b-%d-%Y") ;
+scandir="/home/$USER/" ;
+logfolder="$scandir.clamtk/history/" ;
+logfile="$logfolder$logdate.log" ;
+ignoredir="$scandir.local/share/Trash/" ;
 
-# notif - Start Scan
-# /usr/bin/env SCREEN=:0 DISPLAY=:0 /usr/bin/zenity --notification --text="ClamAV scan has been started"
-# notify-send 'Hello world!' 'ClamAV scan has been started.' --icon=dialog-information
+if [ "$1" != 'test' ]; then
 
+  if ! type -p /usr/bin/on_ac_power &> /dev/null; then
+    # Assume always on AC if on_ac_power doesn't exist
+    onAC=0 ;
+  else
+    onAC=$(/usr/bin/on_ac_power && echo "0" || echo "1") ;
+  fi
 
-if [ $1 != 'test' ]; then
+  if (( onAC != 0  )); then
+    /bin/echo "Not on AC Power. Skipping." ;
+    exit 1 ;
+  fi
 
-  logdate=$(date "+%b-%d-%Y") ;
-  scandir="/home/$USER/" ;
-  logfolder="$scandir.clamtk/history/" ;
-  mkdir -p $logfolder ;
-  logfile="$logfolder$logdate.log" ;
-  ignoredir="$scandir.local/share/Trash/" ;
+  # notif - Start Scan
+  # /usr/bin/env SCREEN=:0 DISPLAY=:0 /usr/bin/zenity --notification --text="ClamAV scan has been started"
+  # notify-send 'Hello world!' 'ClamAV scan has been started.' --icon=dialog-information
 
-  /usr/bin/nice -n 10 /usr/bin/clamscan -ir --log=$logfile --exclude-dir=$ignoredir $scandir &> /dev/null ;
+  mkdir -p "$logfolder" ;
+
+  trap 'rm -f $lockfile; rm -f $logfile; exit' INT TERM EXIT ;
+
+  /usr/bin/nice -n 10 /usr/bin/clamscan -ir --log="$logfile" --exclude-dir="$ignoredir" "$scandir" &> /dev/null ;
   # /bin/echo  "SCANNING"
+
+  trap - INT TERM EXIT ;
+
+else
+
+  trap - INT TERM EXIT ;
+
+  echo "logdate: $logdate"
+  echo "scandir: $scandir"
+  echo "logfolder: $logfolder"
+  echo "logfile: $logfile"
+  echo "ignoredir: $ignoredir"
+  
 fi
 
 # notif - Scan Finished
